22ad877419773500f79382e5d4b13800d78e9dc5
Evgeniy Goryuchkin <goryuchkin@gmail.com>
2021-04-20 14:02:42 +0300

    telemarketing campaigns endpoint, nodemon

--- src/controllers/api/telemarketing.js
+++ src/controllers/api/telemarketing.js
@@ -7,6 +7,23 @@ const router = express.Router()
 
 module.exports = router
 
+router.get('/campaigns.do', async (req, res) => {
+  const paramsValidator = v.create({
+    limit: v.number(),
+    offset: v.number(),
+  })
+
+  try {
+    await paramsValidator(req.params)
+  } catch (e) {
+    log.warn('Bad request %o', e)
+    res.status(400).send()
+    return
+  }
+
+  res.status(200).json([]) // TODO
+})
+
 router.get('/campaigns/:campaign/taskId.json', async (req, res) => {
   const paramsValidator = v.create({
     campaign: v.required(v.number({ min: 1 }))
@@ -20,7 +37,7 @@ router.get('/campaigns/:campaign/taskId.json', async (req, res) => {
     return
   }
 
-  res.json({
+  res.status(200).json({
     taskId: _.random(1000, 9999)
   })
 })
